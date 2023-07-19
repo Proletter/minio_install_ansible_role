ee701846210ae8546c0f7cd6e1d43dcb539de089
Oluwaseun <oluseun@thechumeffect.com>
2023-07-19 19:57:13 +0100

    added tests

--- molecule/resources/tests/test_minio_common.py
+++ molecule/resources/tests/test_minio_common.py
@@ -1,22 +1,11 @@
 import os
 import testinfra.utils.ansible_runner
 
-"""Role testing files using testinfra."""
-
-# Check if host file exists
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
-
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
-
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-# check if minio service has started
+
 def test_minio_service(host):
 
     s = host.service('minio')
